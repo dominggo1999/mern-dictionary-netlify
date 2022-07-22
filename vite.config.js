b307d6f4dc3076ef80acef328ea4ed56d7e98e5b
dominggo1999 <arnolddominggo19@gmail.com>
2022-07-22 23:55:44 +0700

    fix service worker issue

--- vite.config.js
+++ vite.config.js
@@ -21,6 +21,29 @@ export default defineConfig({
         plugins: ['babel-plugin-macros', 'babel-plugin-styled-components'],
       },
     }),
-    VitePWA({ registerType: 'autoUpdate' }),
+    VitePWA({
+      registerType: 'autoUpdate',
+      manifest: {
+        name: 'Dictionary',
+        short_name: 'Dictionary',
+        start_url: '/',
+        scope: '/',
+        icons: [
+          {
+            src: '/favicons/android-chrome-192x192.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: '/favicons/android-chrome-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+        ],
+        theme_color: '#ffffff',
+        background_color: '#ffffff',
+        display: 'fullscreen',
+      },
+    }),
   ],
 });
